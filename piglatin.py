06a5f9dacca8e38cd6a3de465f2fa0b1a5daefb3
mnobile <mnobile@mnobile-mac.local>
2013-11-18 20:04:37 -0500

    pig latin translator final

--- piglatin.py
+++ piglatin.py
@@ -1,6 +1,18 @@
-print "Welcome to the English to Pig Latin translator!"
-original = raw_input('Enter an English word now.')
-if len(original) >= 1 and original.isalpha():
-    print original
+pyg = 'ay' #add ay to end off word
+
+original = raw_input('Enter a word:') #ask for input
+
+if len(original) > 0 and original.isalpha():#getlength and make sure it is alpha
+    word = original.lower() #string word
+    first = word[0] #string to find if first letter is vowel
+    new_word = word + pyg #string to print if it is vowel
+    
+    
+    if first in ['a' or 'e' or 'i' or 'o' or 'u']:#if word begins with vowel
+        print new_word
+    else: #if word doesn't begin with vowel define string new_word again'n print
+        new_word = word[1:] + first + pyg
+        print new_word 
 else:
-    print "Empty or Remove numbers"
+    print 'Empty word or Remove the Numbers' # if  it gets to this
+
