c385149b10cb6833d70588aaaf295eab51d00ad0
mnobile <mnobile@mnobile-mac.local>
2013-11-20 20:07:09 -0500

    sqrt function example

---
+++ sqrt_function.py
@@ -0,0 +1,7 @@
+"""sqrt is a function which takes in a agrument and give the square root of the number...print sqrt simply mean,asking the interpreter to print sqrt function.when you say print,you are printing something to the console screen...you can try this following way
+x=sqrt(13689)
+print x"""
+
+from math import sqrt
+sqrt = 13689
+print sqrt
