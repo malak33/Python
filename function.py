e2eeed901c9129291584d057ed09f938e1952916
mnobile <mnobile@mnobile-mac.local>
2013-11-20 19:37:43 -0500

    common function examples

---
+++ function.py
@@ -0,0 +1,38 @@
+# Set minimum to the min value of any set of numbers on line 3!
+
+minimum = min(1, 3, 33, 99, 304, 1.345, 100, 100.1, 99.9)
+
+print minimum
+
+###########
+# Set maximum to the max value of any set of numbers on line 3!
+
+maximum = max(1, 3, 33, 99, 304, 1.345, 100, 100.1, 99.9)
+
+print maximum
+
+
+#############
+absolute = abs(-42)
+
+print absolute
+
+
+############
+# Print out the types of an integer, a float,
+# and a string on separate lines below.
+print type(42)
+print type(4.2)
+print type('spam')
+print type({'Name':'John Cleese'})
+print type((1,2))
+
+
+#############
+
+
+
+
+
+
+
