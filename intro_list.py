0fab282e217c05af3eefeb1b3bb314847c210fb5
mnobile <mnobile@mnobile-mac.local>
2013-12-26 21:48:18 -0500

    intro to lists

--- intro_list.py
+++ intro_list.py
@@ -121,7 +121,24 @@ print zoo_animals
 
 
 
+####13/13
+inventory = {'gold' : 500,
+'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']}
 
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort() 
+# Here the dictionary access expression takes the place of a list name 
+
+# Your code here
+inventory = {'pocket' : ['seashell', 'strange berry', 'lint']}
+backpack.sort()
+print backpack
+backpack.remove('dagger')
+inventory.append(500 + 50)
 
 
 
