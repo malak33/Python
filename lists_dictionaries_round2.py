0eea60c62fbe1217265acfc074d28fad396e14ef
mnobile <mnobile@rtp-vpn1-365.cisco.com>
2014-01-05 15:01:15 -0500

    lesson 13

--- lists_dictionaries_round2.py
+++ lists_dictionaries_round2.py
@@ -172,3 +172,29 @@ def compute_bill(shopping_list):
     return total
 
 ###part 13/13
+
+"""Remember how to compute a rolling sum? If not, here's a reminder: total += price is the same as total = total + price. Evaluating the right hand side grabs the values of those variables, adds those values, and then stores it back into the variable total."""
+shopping_list = ["banana", "orange", "apple"]
+
+stock = { "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = { "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+def compute_bill(shopping_list):
+    total = 0
+    for x in shopping_list:
+        if stock[x] > 0:
+            total += prices[x]
+            stock[x] -= 1
+        else:
+            print "That item is not in stock"
+    return total
