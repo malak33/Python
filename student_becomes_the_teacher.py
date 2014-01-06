cfa6c3c298bc05991ab3684a6af8414efd53b297
mnobile <mnobile@mnobile-mac.local>
2014-01-05 20:16:40 -0500

    student becomes the teacher

---
+++ student_becomes_the_teacher.py
@@ -0,0 +1,53 @@
+### part 1/9
+
+lloyd = {"name" : "llyod", "homework" : [], "quizzes" : [], "test" : []}
+alice = {"name" : "alice", "homework" : [], "quizzes" : [], "test" : []}
+tyler = {"name" : "tyler", "homework" : [], "quizzes" : [], "test" : []}
+
+
+
+###part 2/9
+
+
+
+
+###part 3/9
+
+
+
+
+
+####part 4/9
+
+
+
+
+
+
+####part 5/9
+
+
+
+
+
+####part 6/9
+
+
+
+
+
+
+####part 7/9
+
+
+
+
+
+####part 8/9
+
+
+
+
+
+
+#####part 9/9
