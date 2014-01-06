1322e1df54d4c9ace6553223260bbc7859d07917
mnobile <mnobile@mnobile-mac.local>
2014-01-05 21:13:14 -0500

    student becomes the teacher

--- student_becomes_the_teacher.py
+++ student_becomes_the_teacher.py
@@ -112,7 +112,36 @@ def average(lst):
 
 ####part 6/9
 
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
 
+# Add your function below!
+def average(lst):
+    return  float(sum(lst)) / len(lst)
+    
+def get_average(student):
+    homework = average(student["homework"]) * .10
+    quizzes = average(student["quizzes"]) * .30
+    tests = average(student["tests"]) * .60
+    weighted_average = homework + quizzes + tests
+    return weighted_average
+    
 
 
 
