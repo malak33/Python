0182e0247d18b19d060ed1c5c97194f595ef2406
Michael Nobile <nobility33@gmail.com>
2015-11-19 13:09:03 -0500

    how to use with statement

---
+++ createWithFile.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+# createWithFile.py - an example of how to open a file using with
+
+filename, outfile = 'twain.txt', 'twain_out.txt'
+
+try:
+    with open(filename) as f_in, open(outfile, 'w') as f_out:
+        for line in f_in:
+            f_out.write(line)
+except IOError as e:
+    print(e)
+
+with open(outfile) as f:
+    print(len(f.readlines()))
+
